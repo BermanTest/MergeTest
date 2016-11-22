ba4741529c4a122e3c0280b1b5f3ec21260caa67
Karol Gotkowski <Karol Gotkowski@gmx.de>
2016-11-22 16:45:04 +0100

    f

---
+++ MergeTest001/src/Test001.java
@@ -0,0 +1,11 @@
+
+public class Test001
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		// IN dieses Projekt soll gemerged werden. Sprich das ist der Hauptpfad.
+	}
+
+}
